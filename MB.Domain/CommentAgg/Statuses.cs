c4f44b8392260abb24fe96accf48db55fa1701e3
erfan aboei <erfan.a3840@gmail.com>
2022-02-01 17:20:18 +0330

    Comment Domain Implemented

---
+++ MB.Domain/CommentAgg/Statuses.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MB.Domain.CommentAgg
+{
+    public static class Statuses
+    {
+        public const int NewComment = 0;
+        public const int ConfirmedComment = 1;
+        public const int CanceledComment = 2;
+
+    }
+}
